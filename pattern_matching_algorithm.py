01e678784aedcc99b089ac220761da83f7c1a09c
Munieshwar Ramdass <mramdass@nyu.edu>
2015-11-22 00:01:12 -0500

    Pattern Matching Algorithm File Initial Commit
    We require one file to detect patterns for any workflows, even for those
    we do not know of.

---
+++ pattern_matching_algorithm.py
@@ -0,0 +1,11 @@
+import repository_lib, repository_tool
+
+# It starts here:
+#       One file
+#       Do not check on individual workflows... makes no sense
+#       Checks to detect a behavior or pattern
+#       Functions should include:
+#           function to recurse to previous commit
+#           using imported functions to grab metadata
+#           function that has our algorithm to detect a pattern * the main one
+#       The imported libraries have most or all of the TUF metadata grabbing stuff

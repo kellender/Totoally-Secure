f9fa436b3318d79b7be3da17f9644f1d78709e8b
Fernando Maymi <fernando.maymi@nyu.edu>
2015-12-09 21:02:27 -0500

    Make minor adjustments to check.py
    Check.py now assumes strings from the permission list to play better
    with aclEdit.py and json writing.
    Check.py contains nonfunctioning code to implement indefinite begin
    and end times on time restricted permission.  This needs to be fixed

--- check.py
+++ check.py
@@ -9,52 +9,76 @@
 <Returns>
     Dictionary with commits as keys, permission violations as values
 
+<Issues>
+    Time restricted permissions do not currently support None type. 
+    An implementation was attempted but results in an error.
 '''
+
 import time
+import json
 from datetime import datetime
 from dateutil.parser import parse
 from pytz import utc
 
 def checker(metadata, permissions):
     violations = {}
+    if 'all' not in permissions:
+        permissions['all'] = {}
     for commit in metadata:
         errStr = ''
         author = metadata[commit]['author']
-        if author not in permissions:
-            errStr += ' Author not in permissions.'
-        elif permissions[author]['write'] == False or 'write' not in permissions[author]:
-            errStr += ' Author does not have permission to write.'
+        if 'write' in permissions['all'] and permissions['all']['write'] == True:
+            pass
+        elif author not in permissions:
+            errStr += 'Author not in permissions. '
+        elif 'write' not in permissions[author] or permissions[author]['write'] == False:
+            errStr += 'Author does not have permission to write. '
         elif type(permissions[author]['write']) == datetime:
             time_stamp = parse(metadata[commit]['author_timestamp'])
-            print time_stamp
-            if time_stamp < permissions[author]['commit'][0]:
-                errStr += ' Write occured before author had write permission.'
-            elif time_stamp > permissions[author]['commit'][1]:
-                errStr += " Write occured after author's write permission expired."
+            begin = parse(permissions[committer]['write'][0])
+            end = parse(permissions[committer]['write'][1])
+            if begin == None:
+                assert time_stamp > begin
+            if end == None:
+                assert time_stamp < end
+            if time_stamp < begin:
+                errStr += 'Write occured before author had write permission. '
+            elif time_stamp > end:
+                errStr += "Write occured after author's write permission expired. "
 
         committer = metadata[commit]['committer']
-        if committer not in permissions:
-            errStr += ' Committer not in permissions.'
+        if 'commit' in permissions['all'] and permissions['all']['commit'] == True:
+            pass
+        elif committer not in permissions:
+            errStr += 'Committer not in permissions. '
         elif 'commit' not in permissions[committer] or permissions[committer]['commit'] == False:
-            errStr += ' Committer does not have permission to commit.'
+            errStr += 'Committer does not have permission to commit. '
         elif type(permissions[committer]['commit']) == tuple:
             time_stamp = parse(metadata[commit]['commit_timestamp'])
-            print time_stamp
+            if permissions[committer]['commit'][0] == None:
+                assert time_stamp > permissions[committer]['commit'][0]
+            if permissions[committer]['commit'][1] == None:
+                assert time_stamp < permissions[committer]['commit'][1]
             if time_stamp < permissions[committer]['commit'][0]:
-                errStr += ' Commit occured before committer had commit permission.'
+                errStr += 'Commit occured before committer had commit permission. '
             elif time_stamp > permissions[committer]['commit'][1]:
-                errStr += " Commit occured after committer's commit permission expired."
+                errStr += "Commit occured after committer's commit permission expired. "
 
         if committer in permissions and 'merge' in metadata[commit]['type']:
-            if permissions[committer]['merge'] == False or 'merge' not in permissions[committer]:
-                errStr += ' Committer does not have permission to merge.'
+            if 'merge' in permissions['all'] and permissions['all']['merge'] == True:
+                pass
+            elif 'merge' not in permissions[committer] or permissions[committer]['merge'] == False:
+                errStr += 'Committer does not have permission to merge. '
             elif type(permissions[committer]['merge']) == datetime:
                 time_stamp = parse(metadata[commit]['commit_timestamp'])
-                print time_stamp
+                if permissions[committer]['merge'][0] == None:
+                    assert time_stamp > permissions[committer]['merge'][0]
+                if permissions[committer]['merge'][1] == None:
+                    assert time_stamp < permissions[committer]['merge'][1]
                 if time_stamp < permissions[committer]['merge'][0]:
-                    errStr += ' Merge occured before committer had merge permission.'
+                    errStr += 'Merge occured before committer had merge permission. '
                 elif time_stamp > permissions[committer]['merge'][1]:
-                    errStr += " Merge occured after committer's merge permission expired."
+                    errStr += "Merge occured after committer's merge permission expired. "
         
         if errStr == '':
             violations[commit] = 'OK'
@@ -63,14 +87,10 @@ def checker(metadata, permissions):
     return violations
 
 if __name__ == '__main__':
-    a = utc.localize(datetime.now())
-    time.sleep(10)
-    b = utc.localize(datetime.now())
+    with open('acl.json', 'r') as f:
+        acl = json.load(f)
     dat = {
-        '1':{'author':'Juan', 'committer':'Pablo', 'type':'merge', 'commit_timestamp': "2015-11-20 08:42:27 +0100"},
+        '1':{'author':'Tacos', 'committer':'Juan', 'type':'commit', 'commit_timestamp': "2015-11-20 08:42:27 +0100"},
+        '2':{'author':'Juan', 'committer':'Tacos', 'type':'merge', 'commit_timestamp': "2015-11-20 08:42:27 +0100"},
            }
-    acl = {
-        'Juan':{'write':True},
-        'Pablo':{'commit':(a,b),'merge':True}
-            }
     print checker(dat, acl)
